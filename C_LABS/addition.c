e8265bb8d4d98c54cec84a34df9a724442aa745c
padalgharearjun <113876493+padalgharearjun@users.noreply.github.com>
2022-09-21 11:11:55 +0530

    sum output syntax correction

--- C_LABS/addition.c
+++ C_LABS/addition.c
@@ -6,15 +6,15 @@ int main (void)
 int sum=0;
 int n1=0;
 int n2=0;
-printf("Enter first number:\n")
+printf("Enter first number:\n");
 scanf("%d",&n1);
 
-printf("Enter second number:\n")
+printf("Enter second number:\n");
 scanf("%d",&n2);
 
 sum=n1+n2;
 
-printf("Sum of %d and %d is:%d",sum,n1,n2);
+printf("Sum of %d and %d is:%d",n1,n2,sum);
 
 return 0;
 }
