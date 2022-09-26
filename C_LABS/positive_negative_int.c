74078d83a6f743898ab909a9f40e312f0a2f3624
Arjun Padalghare <113876493+padalgharearjun@users.noreply.github.com>
2022-09-26 19:29:44 +0530

    Create positive_negative_int.c

---
+++ C_LABS/positive_negative_int.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    
+    if (num==0)
+    {
+        printf("Neither positive nor negative");
+    }
+    else if (num>0 & num!=0)
+    {
+        printf("Positive");
+    }
+    else if (num < 0 & num!=0)
+    {
+      printf("Negative");
+    }
+   
+
+    return 0;
