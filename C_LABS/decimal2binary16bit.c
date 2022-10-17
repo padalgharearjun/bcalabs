015824e20df67a46bd8dea15d5e0b2a000be32ae
Arjun Padalghare <113876493+padalgharearjun@users.noreply.github.com>
2022-10-17 21:15:58 +0530

    Update decimal2binary16bit.c

--- C_LABS/decimal2binary16bit.c
+++ C_LABS/decimal2binary16bit.c
@@ -7,25 +7,32 @@ Program to convert any integer decimal number (max. 65535) to binary (upto 16 bi
 
 int main()
 {
-    int n=65535;
+    int n=0;
     int r=0;
+    int count=0;
     int arr[16]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     
+    printf("Enter any decimal number(0 to 65535:");
+    scanf("%d",&n);
     
-    for (int i=0;i<16;i++){
+    if(n<=65535 & n>=0){
+    
+    for (int i=0;n>0;i++){
     
     r=n%2;
     arr[i]=r;
     printf("%d %d \n",n,r);
     n=n/2;
-    
+    count++;
     }
     
     printf("16bit binary output:");
-    for(int i=15;i>=0;i--){
+    for(int i=count;i>=0;i--){
         printf("%d",arr[i]);
     }
-    
+    }else{
+        printf("Entered Decimal number is invalid.");
+    }
 
     return 0;
 }
