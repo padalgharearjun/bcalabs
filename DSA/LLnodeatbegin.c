ddaaf0a45a18688ff25eb55370c9890560cfa811
Arjun Padalghare <113876493+padalgharearjun@users.noreply.github.com>
2022-12-11 12:24:31 +0000

    Added LL node at begining source file

---
+++ DSA/LLnodeatbegin.c
@@ -0,0 +1,33 @@
+
+//Program to illustrate the node insertion at the begining of the linked list.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//created node structure with one int data and link pointer poinintg next node. 
+typedef struct node
+{
+    int data;
+    struct node *link;
+}node;
+
+int main()
+{
+    //First node
+    node * head;
+    head = (node * ) malloc(sizeof(node));
+    head->data=1;
+    head->link=NULL;
+
+    
+
+
+
+
+
+
+
+
+
+    return 0;
+}
